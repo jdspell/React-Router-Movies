a0e52c1748e71843602268ae469454a63fde5146
jdspell <spelljd15@gmail.com>
2019-02-26 11:18:13 -0500

    added save  movie functionality to movie app

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,6 +26,7 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
+
   // Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
   //   if(this.props.match.params.id !== newProps.match.params.id){
@@ -42,12 +43,16 @@ export default class Movie extends Component {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
-    const { title, director, metascore, stars } = this.state.movie;
+    
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
-      <div className="save-button">Save</div>
+      <div 
+        className="save-button"
+        onClick={() => this.props.addToSavedList(this.state.movie)}
+      >
+        Save
+      </div>
       </div>
     );
   }
